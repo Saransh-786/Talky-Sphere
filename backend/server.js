0791c3edb8d74bd5ea5244f4bf9c6d70f12267a9
Saransh-786 <saranshsaxena01@gmail.com>
2024-08-09 04:25:32 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 
 const express = require("express");
+const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 
-const app=express()
 
+const app=express()
+dotenv.config();
 
 app.get('/',(req,res)=>{
     res.send("Api is running");
@@ -20,6 +22,7 @@ app.get('/api/chat/:id',(req,res)=>{
     res.send(singlechat);
 })
 
+const PORT = process.env.PORT || 5000
 app.listen(5000,()=>{
-    console.log("Server started on 5000");
+    console.log(`Server started on PORT ${PORT}`);
 });
