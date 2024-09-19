3d3883c5cb7ad38fb3faf6f3e78139b09fefd08b
Saransh-786 <saranshsaxena01@gmail.com>
2024-09-19 19:44:02 +0530

    i m back

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-
 import './App.css';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import ChatPage from './Pages/ChatPage';
+import HomePage from './Pages/HomePage';
+
 
 function App() {
   return (
     <div className="App">
-        Hello
+      <Route path="/" component={HomePage}/>
+       <Route path="/chats" component={ChatPage} /> 
     </div>
   );
 }
