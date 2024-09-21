851be400257750906603b4b7feccae01293e2c0e
Saransh-786 <saranshsaxena01@gmail.com>
2024-09-22 01:56:05 +0530

    Form

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,7 +1,50 @@
+import { Box,
+    Container,
+    Tab,
+    TabList,
+    TabPanel,
+    TabPanels,
+    Tabs,
+    Text, } from "@chakra-ui/react";
 import React from "react";
+import Login from "../Components/Authentication/Login";
+import Signup from "../Components/Authentication/SignUp"
+
 
 const HomePage = () =>{
-    return <div>Home</div>
+    return <Container
+        maxW="xl" centerContent>
+        <Box
+            d="flex"
+            justifyContent="center"
+            p={3}
+            bg={"white"}
+            w="100%"
+            m="40px 0 15px 0"
+            borderRadius="lg"
+            borderWidth="1px"
+           > 
+            <Text fontSize="xx-large" fontFamily="Work sans" color="black">
+                Talky-Sphere
+            </Text>
+        </Box>
+        <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login/>
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
 }
 
 export default HomePage
