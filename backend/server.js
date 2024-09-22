ee6831a7688858ed06d11bae3c252f1fa2a96be1
Saransh-786 <saranshsaxena01@gmail.com>
2024-09-22 14:26:49 +0530

    Db install

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB=require("./Config/Db")
 
-
-const app=express()
 dotenv.config();
+
+connectDB();
+const app=express()
+
+
 
 app.get('/',(req,res)=>{
     res.send("Api is running");
