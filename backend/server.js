657428c2e9fbe87ce1210c6afb02f6797e32bc42
Saransh-786 <saranshsaxena01@gmail.com>
2024-09-23 00:01:18 +0530

    handler

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB=require("./Config/Db")
+const userRoutes=require("./Routes/UserRoutes")
 
 dotenv.config();
 
@@ -14,16 +15,7 @@ app.get('/',(req,res)=>{
     res.send("Api is running");
 });
 
-app.get('/api/chat',(req,res)=>{
-    res.send(chats);
-});
-
-
-//to find single chat in data
-app.get('/api/chat/:id',(req,res)=>{
-    const singlechat=chats.find((c)=>c._id==req.params.id);
-    res.send(singlechat);
-})
+app.use('/api/user',userRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(5000,()=>{
